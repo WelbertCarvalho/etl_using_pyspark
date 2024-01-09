f9b91bafa6d80fe27b0139085c8ae2e8163309eb
welbert <welbertsilveiracarvalho@gmail.com>
2024-01-08 21:40:58 -0300

    Refactoring some parts of the code

--- transformation.py
+++ transformation.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     extract_obj = Data_extractor()
     data = extract_obj.get_json_data(
         url = 'https://economia.awesomeapi.com.br/json/daily/EUR-BRL',
-        num_days = 1000
+        num_days = 20
     )
 
     # Initializing a spark management instance
